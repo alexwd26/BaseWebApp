1218590413e423aed4acf347c6dd63d829fd77c7
alexwd26 <alexwd26@hotmail.com>
2025-04-30 10:30:37 -0300

    ss

--- Waiter_app/routers/orders.py
+++ Waiter_app/routers/orders.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel, ValidationError, field_validator
 from typing import Optional, List, Annotated
 from db import get_db
